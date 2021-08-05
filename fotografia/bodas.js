a7818056d7a8ead26227fcfdb3cedf1694c5ad80
zelevf <fernandoveleze@gmail.com>
2021-08-05 15:19:01 -0300

    Estilos y albumes de fotos

---
+++ fotografia/bodas.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentloaded', mostrarHTML)
+
+function obtenerFotos() {
+    const url = './bodas.json';
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            mostrarHTML(resultado)
+        })
+} 
+obtenerFotos();
+
+function mostrarHTML(fotos) {
+    const fotosNew = document.querySelector('.packFotos');
+
+    fotos.forEach(fotos => {
+        const foto = document.createElement('img');
+        foto.src = fotos.url;
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.appendChild(foto);
+
+        fotosNew.appendChild(card);
+    });
+}
