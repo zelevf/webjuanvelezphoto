3055f99b96f896302d12517cbc16a6f75315b5f9
zelevf <fernandoveleze@gmail.com>
2021-08-09 22:38:37 -0300

    Cambios menu mobile, se reduce

---
+++ app.js
@@ -0,0 +1,16 @@
+const plus = document.querySelector('.plusMobile');
+const navegadorMobile = document.querySelector('.navegacion');
+
+
+plus.addEventListener('click', () => {
+
+    if (navegadorMobile.style.display == 'flex') {
+        navegadorMobile.style.display = 'none';
+        plus.style.transform = 'rotate(0deg)';
+        plus.style.transition = '.3s';
+    } else {
+        navegadorMobile.style.display = 'flex';
+        plus.style.transform = 'rotate(45deg)';
+        plus.style.transition = '.3s';
+    }
+})
