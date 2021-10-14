d4192053129c8a7d5b8990b9b39d4bbabdb7fe41
zelevf <fernandoveleze@gmail.com>
2021-10-13 22:11:17 -0300

    Form de About Me

---
+++ app2.js
@@ -0,0 +1,38 @@
+const plus = document.querySelector('.plusMobile');
+const navegadorMobile = document.querySelector('.navegacion');
+
+plus.addEventListener('click', () => {
+
+    if (navegadorMobile.style.display == 'flex') {
+        navegadorMobile.style.display = 'none';
+        plus.style.transform = 'rotate(0deg)';
+        plus.style.transition = '.2s';
+    } else {
+        navegadorMobile.style.display = 'flex';
+        plus.style.transform = 'rotate(45deg)';
+        plus.style.transition = '.2s';
+    }
+})
+
+
+// --------------------------- APAREZCA BOTON IR AL PRINCIPIO ------------------------
+
+
+
+const fotoPortada = document.querySelector(".aboutmeCover");
+
+window.addEventListener('scroll', () => {
+    const seccionPortada = document.querySelector('.aboutmeBody');
+    const ubicacion = seccionPortada.getBoundingClientRect();
+
+    // MOBILE 
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        if(ubicacion.top > -370) {
+            fotoPortada.style.opacity = 1;
+            fotoPortada.style.transitionDuration = ".3s";
+        } else {
+            fotoPortada.style.opacity = 0;
+            fotoPortada.style.transitionDuration = ".3s";
+        };
+    } 
+})
